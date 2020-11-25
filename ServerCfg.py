a2f1f95f31572395898c0d626e5a0057181f33d4
jakob753951 <jlm1999@live.dk>
2020-11-25 10:08:28 +0000

    removed config files

--- ServerCfg.py
+++
@@ -1,9 +0,0 @@
-class ServerCfg:
-	def __init__(self, cate_personal_vc, chan_personal_vc, chan_message_log, chan_member_log, role_remind, chan_remind, confirm_reaction):
-		self.cate_personal_vc = cate_personal_vc
-		self.chan_personal_vc = chan_personal_vc
-		self.chan_message_log = chan_message_log
-		self.chan_member_log = chan_member_log
-		self.role_remind = role_remind
-		self.chan_remind = chan_remind
-		self.confirm_reaction = confirm_reaction
