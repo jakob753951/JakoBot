17559011912b16319de30fab7b2719455e16ab61
jakob753951 <jlm1999@live.dk>
2021-01-16 04:32:23 +0000

    finished up rewards

--- available_cogs/Rewards.py
+++ available_cogs/Rewards.py
@@ -22,46 +22,67 @@ class Rewards(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 		self.cfg = load_config('config.json')
-		self.load_cooldowns()
 
-	def load_cooldowns(self):
+	def get_cooldown(self, guild_id, user_id, reward_id):
 		with open('data/RewardCooldowns.json') as cooldowns_file:
-			self.cooldowns = json.loads(cooldowns_file.read())
+			cooldowns = json.loads(cooldowns_file.read())
 
-	def save_cooldowns(self):
+		try:
+			time_string = cooldowns[str(guild_id)][str(user_id)][str(reward_id)]
+			return datetime.strptime(time_string, fmt)
+		except:
+			return datetime.min
+
+	def set_cooldown(self, guild_id, user_id, reward_id):
+		with open('data/RewardCooldowns.json') as cooldowns_file:
+			cooldowns = json.loads(cooldowns_file.read())
+		if str(guild_id) not in cooldowns:
+			cooldowns[str(guild_id)] = {}
+		if str(user_id) not in cooldowns[str(guild_id)]:
+			cooldowns[str(guild_id)][str(user_id)] = {}
+		cooldowns[str(guild_id)][str(user_id)][str(reward_id)] = datetime.strftime(datetime.utcnow(), fmt)
 		with open('data/RewardCooldowns.json', 'w') as cooldowns_file:
-			cooldowns_file.write(json.dumps(self.cooldowns))
+			cooldowns_file.write(json.dumps(cooldowns, indent=4))
 
-	def load_rewards(self):
+	def get_reward(self, guild_id, reward_id):
 		with open('data/Rewards.json') as rewards_file:
-			self.rewards = json.loads(rewards_file.read())
+			rewards = json.loads(rewards_file.read())
+
+		if not str(guild_id) in rewards:
+			raise ValueError('Rewards have not been registered for this server.')
+		if not str(reward_id) in rewards[str(guild_id)]:
+			raise ValueError('Invalid reward id. Please try again with a correct one.')
+
+		return rewards[str(guild_id)][str(reward_id)]
 
 	@commands.guild_only()
-	@commands.command(name='AddCurrency', aliases=['addcurrency'])
+	@commands.command(name='Reward', aliases=['reward', 'award'])
 	async def reward(self, ctx, member: discord.Member, reward_id: int):
 		msg_cfg = self.cfg.servers[ctx.guild.id]
+		try:
+			reward = self.get_reward(ctx.guild.id, reward_id)
+		except ValueError as e:
+			await ctx.send(repr(e))
+			return
 
-		self.load_cooldowns()
-		self.load_rewards()
+		last_reward_time = self.get_cooldown(ctx.guild.id, member.id, reward_id)
 
-		time_string = self.cooldowns[ctx.guild.id][ctx.author.id][reward_id]
-		reward = self.rewards[ctx.guild.id][reward_id]
-
-		last_reward_time = datetime.strptime(time_string, fmt)
 		available_time = last_reward_time + timedelta(hours=reward['cooldown_in_hours'])
 
 		cur_time = datetime.utcnow()
 		if not available_time < cur_time:
-			desc = f'Reward not yet available. Will be available'
+			desc = f'Reward not yet available.'
 			embed = discord.Embed(color=0x0000ff, description=desc, timestamp=available_time)
-			embed.set_footer(text='Next reward will be available at: ')
-			ctx.send(embed=embed)
+			embed.set_footer(text='Next reward will be available: ')
+			await ctx.send(embed=embed)
 			return
 
 		amount = reward['amount']
 
+		self.set_cooldown(ctx.guild.id, member.id, reward_id)
 		await manager.addToMemberBalance(ctx.guild.id, member.id, amount)
-		await transaction_log(self.bot, msg_cfg, ctx.author, amount, title=f'User was rewarded for task number {reward_id}: {reward.name}')
+
+		await transaction_log(self.bot, msg_cfg, member, amount, title=f"User was rewarded by {ctx.author.name} for task number {reward_id}: {reward['name']}")
 		await ctx.message.add_reaction(msg_cfg.react_confirm)
 
 
