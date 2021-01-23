5380ece3cbd73dcd3c5beec31ab3c500bfa5ad94
jakob753951 <jlm1999@live.dk>
2021-01-23 23:19:31 +0000

    Added Birthday Module

---
+++ available_cogs/Birthday.py
@@ -0,0 +1,41 @@
+import asyncio
+import discord
+from discord.ext import commands
+from Configuration import *
+import CurrencyManager as manager
+from CurrencyUtils import *
+from CustomChecks import *
+
+requirements = {
+	'general': [],
+	'server': [
+		'currency_name_singular',
+		'currency_name_plural'
+	]
+}
+
+class Birthday(commands.Cog):
+	def __init__(self, bot):
+		self.bot = bot
+		self.cfg = load_config('config.json')
+
+	@commands.guild_only()
+	@commands.command(name='Birthday', aliases=['birthday', 'birthdaypresent', 'birthdaygift'])
+	async def brithday(self, ctx, member: discord.Member):
+		guild_cfg = self.cfg.servers[ctx.guild.id]
+
+		amount = 500
+
+		desc=f"Here's your birthday present. We hope you like it!\nIt's **{amount}** {pluralise(guild_cfg, amount)}!!!"
+		embed = discord.Embed(color=0x00ff00, description=desc, timestamp=datetime.utcnow())
+		embed.set_footer(text='Happy birthday')
+
+		await asyncio.gather(
+			manager.addToMemberBalance(ctx.guild.id, member.id, amount),
+			transaction_log(self.bot, guild_cfg, member, amount, title=f"User got a birthday present."),
+			member.send(embed=embed)
+		)
+
+
+def setup(bot):
+	bot.add_cog(Birthday(bot))
