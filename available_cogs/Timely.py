181fca484130a5d3ee6692f337417d28b0a2e9d7
jakob753951 <jlm1999@live.dk>
2021-01-25 22:12:23 +0000

    Make timely optinally channel specific. General improvements to Timely.py

--- available_cogs/Timely.py
+++ available_cogs/Timely.py
@@ -1,6 +1,7 @@
-import asyncio
+from asyncio import gather, sleep
 from datetime import timedelta
 import discord
+from discord import Embed
 from discord.ext import commands
 from Configuration import *
 import CurrencyManager as manager
@@ -13,7 +14,8 @@ requirements = {
 		'currency_name_singular',
 		'currency_name_plural',
 		'timely_cooldown_hours',
-		'timely_amount'
+		'timely_amount',
+		'chans_timely'
 	]
 }
 
@@ -48,6 +50,23 @@ class Timely(commands.Cog):
 	async def timely(self, ctx):
 		guild_cfg = self.cfg.servers[ctx.guild.id]
 
+		if len(guild_cfg.chans_timely) != 0 and ctx.channel.id not in guild_cfg.chans_timely:
+			chans = [ctx.guild.get_channel(chan_id) for chan_id in guild_cfg.chans_timely]
+			if len(guild_cfg.chans_timely) == 1:
+				desc = f'Please go to {chans[0].mention} for all of your botting needs!'
+			else:
+				desc = f'Please go to one of the following channels for all of your botting needs:'
+				for chan in chans:
+					desc += '\n' + chan.mention
+
+			sent_msg, _ = await gather(
+				ctx.send(embed=Embed(title='Incorrect channel', description=desc, color=0xff0000)),
+				ctx.message.delete()
+			)
+			await sleep(5)
+			await sent_msg.delete()
+			return
+
 		last_reward_time = get_cooldown(ctx.guild.id, ctx.author.id)
 
 		available_time = last_reward_time + timedelta(hours=guild_cfg.timely_cooldown_hours)
@@ -55,13 +74,13 @@ class Timely(commands.Cog):
 		cur_time = datetime.utcnow()
 		if not available_time < cur_time:
 			title = f"Timely isn't available yet."
-			embed = discord.Embed(color=0xff0000, title=title, timestamp=available_time)
+			embed = Embed(color=0xff0000, title=title, timestamp=available_time)
 			embed.set_footer(text='Next timely will be available: ')
-			responses: list = await asyncio.gather(
+			responses: list = await gather(
 				ctx.send(embed=embed),
 				ctx.message.delete()
 			)
-			await asyncio.sleep(5)
+			await sleep(5)
 			await responses[0].delete()
 			return
 
@@ -69,10 +88,10 @@ class Timely(commands.Cog):
 
 		set_cooldown(ctx.guild.id, ctx.author.id)
 		desc=f"{ctx.author.mention} You've claimed your {amount} {pluralise(guild_cfg, amount)}."
-		embed = discord.Embed(color=0x00ff00, description=desc, timestamp=datetime.utcnow() + timedelta(hours=guild_cfg.timely_cooldown_hours))
+		embed = Embed(color=0x00ff00, description=desc, timestamp=datetime.utcnow() + timedelta(hours=guild_cfg.timely_cooldown_hours))
 		embed.set_footer(text='You can claim again:')
 
-		await asyncio.gather(
+		await gather(
 			manager.addToMemberBalance(ctx.guild.id, ctx.author.id, amount),
 			ctx.message.delete(),
 			transaction_log(self.bot, guild_cfg, ctx.author, amount, title=f"User claimed their timely."),
