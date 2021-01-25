41ba7f311638cb1ddf0843102294afa5238d27cd
jakob753951 <jlm1999@live.dk>
2021-01-25 02:43:04 +0000

    Made BulkRoles.py use gather

--- available_cogs/BulkRoles.py
+++ available_cogs/BulkRoles.py
@@ -1,3 +1,4 @@
+import asyncio
 import discord
 from discord.ext import commands
 
@@ -7,21 +8,13 @@ class Confessions(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 
+	@commands.guild_only()
 	@commands.command(name='Bulk Add Roles', aliases=['addroles', 'bulkaddroles'])
-	async def bulk_add_roles(self, ctx, *role_names):
-		# Check if DM
-		if isinstance(ctx.channel, discord.channel.DMChannel):
-			await ctx.send('This command cannot be used in DMs')
-			return
-
-		perms = ctx.guild.roles[0].permissions
-
-		# Add roles
-		for role_name in role_names:
-			await ctx.guild.create_role(name=role_name, reason='Bulk Add', permissions=perms)
-
-		# Send feedback
-		await ctx.send(embed=discord.Embed(colour=discord.Colour.green(), title='Roles have been created'))
+	async def bulk_add_roles(self, ctx, *, role_names):
+		await asyncio.gather(
+			ctx.send(embed=discord.Embed(colour=discord.Colour.green(), title='Roles have been created')),
+			*(ctx.guild.create_role(name=role_name, reason='Bulk Add', permissions=ctx.guild.default_role.permissions) for role_name in role_names.split(','))
+		)
 
 
 def setup(bot):
