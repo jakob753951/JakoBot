a4f8fa71c30d114cbd5070d4fadebd45c1223d44
jakob753951 <jlm1999@live.dk>
2021-02-06 18:50:06 +0000

    SHIT WORK

--- available_cogs/PlayFile.py
+++ available_cogs/PlayFile.py
@@ -1,15 +1,16 @@
 import discord
 from discord.ext import commands
 import asyncio
+from asyncio import gather
+
+requirements = {'general': [], 'server': ['react_confirm']}
 
 stop_event = asyncio.Event()
 
 def after(error):
 	if error:
 		print(error)
-
-	loop = asyncio.get_event_loop()
-	loop.call_soon_threadsafe(lambda: stop_event.set())
+	stop_event.set()
 
 async def play_file(channel, filename, volume = 1):
 	voice = channel.guild.voice_client
@@ -29,7 +30,20 @@ class PlayFile(commands.Cog):
 		if message.author.id != 250667785368109056:
 			return
 
-		if 'i have an idea' in message.content.lower():
-			await play_file(message.author.voice.channel, 'https://ladegaardmoeller.dk/main/audio/OhNo.mp3', 1)
+		idea_strings = ['i have an idea', 'i have got an idea', 'i gots an idea', 'i has an idea', 'i got an idea', 'ive got an idea', "i've got an idea"]
+		if any(s in message.content.lower() for s in idea_strings):
+			await gather(
+				play_file(message.author.voice.channel, 'https://ladegaardmoeller.dk/audio/OhNo.mp3', .5),
+				message.channel.send('https://tenor.com/view/idea-light-bulb-lightbulb-gif-5187169')
+			)
+
+	@commands.command(name='Play', aliases=['P'])
+	async def play(self, ctx, url: str, volume: float = .5):
+		await gather(
+			play_file(ctx.author.voice.channel, url, volume),
+			ctx.message.add_reaction(self.cfg.servers[ctx.guild.id].react_confirm)
+		)
 
 
+def setup(bot):
+	bot.add_cog(PlayFile(bot))
