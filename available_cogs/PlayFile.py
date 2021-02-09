2b81300adc6a4416a7c818d9bb36304c0c7605d7
jakob753951 <jlm1999@live.dk>
2021-02-09 18:49:37 +0000

    Fixed not resetting event flag, preventing multiple play commands. Also converted volume arg to percent

--- available_cogs/PlayFile.py
+++ available_cogs/PlayFile.py
@@ -13,13 +13,14 @@ def after(error):
 		print(error)
 	stop_event.set()
 
-async def play_file(channel: discord.VoiceChannel, source_uri: str, volume: float = .5):
+async def play_file(channel: discord.VoiceChannel, source_uri: str, volume: float):
 	voice = channel.guild.voice_client
 	if not voice:
 		voice = await channel.connect()
 	audio = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(source_uri), volume)
 	voice.play(audio, after=after)
 	await stop_event.wait()
+	stop_event.clear()
 	await voice.disconnect()
 
 class PlayFile(commands.Cog):
@@ -28,9 +29,9 @@ class PlayFile(commands.Cog):
 		self.cfg = load_config()
 
 	@commands.command(name='Play', aliases=['P'])
-	async def play(self, ctx, source: str, volume: float = .5):
+	async def play(self, ctx, source: str, volume_percent: int = 25):
 		await gather(
-			play_file(ctx.author.voice.channel, source, volume),
+			play_file(ctx.author.voice.channel, source, min(1, volume_percent/100)),
 			ctx.message.add_reaction(self.cfg.servers[ctx.guild.id].react_confirm)
 		)
 
