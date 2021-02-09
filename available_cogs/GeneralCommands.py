72ccc76479923db1729446a0a94835a1288cb54b
jakob753951 <jlm1999@live.dk>
2021-02-09 21:07:46 +0000

    Small improvements to Say and SayNoEmbed

--- available_cogs/GeneralCommands.py
+++ available_cogs/GeneralCommands.py
@@ -29,35 +29,27 @@ class GeneralCommands(commands.Cog):
 	@commands.command(name='Say')
 	async def say(self, ctx, *args):
 		try:
-			to_convert = args[0]
-			cvt = TextChannelConverter()
-			channel = await cvt.convert(ctx, to_convert)
-			message = ' '.join(args[1:])
+			channel = await TextChannelConverter().convert(ctx, args[0])
+			args = args[1:]
 			await ctx.message.add_reaction(self.cfg.servers[ctx.guild.id].react_confirm)
 		except Exception as e:
 			channel = ctx.channel
-			message = ' '.join(args)
 			await ctx.message.delete()
 
-		message = message.replace('\\n', '\n')
-
+		message = ' '.join(args).replace('\\n', '\n')
 		await channel.send(embed=Embed(description=message, color=0xff8000), files=ctx.message.attachments)
 
 	@commands.command(name='SayNoEmbed', aliases=['NoEmbedSay'])
 	async def say_no_embed(self, ctx, *args):
 		try:
-			to_convert = args[0]
-			cvt = TextChannelConverter()
-			channel = await cvt.convert(ctx, to_convert)
-			message = ' '.join(args[1:])
+			channel = await TextChannelConverter().convert(ctx, args[0])
+			args = args[1:]
 			await ctx.message.add_reaction(self.cfg.servers[ctx.guild.id].react_confirm)
 		except Exception as e:
 			channel = ctx.channel
-			message = ' '.join(args)
 			await ctx.message.delete()
 
-		message = message.replace('\\n', '\n')
-
+		message = ' '.join(args).replace('\\n', '\n')
 		await channel.send(message, files=ctx.message.attachments)
 
 
