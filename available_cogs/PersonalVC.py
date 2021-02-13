28e0660932b78f551f45832ae918c79f3937cd5b
jakob753951 <jlm1999@live.dk>
2021-02-13 13:20:52 +0000

    added argument for creation of channels

--- available_cogs/PersonalVC.py
+++ available_cogs/PersonalVC.py
@@ -35,7 +35,7 @@ class PersonalVC(commands.Cog):
 
 		# If we reach this point, the user has connected to the 'Create personal VC' channel
 		# So we create the channels for them and move them to the VC
-		await self.create_channels()
+		await self.create_channels(member)
 
 	async def create_channels(self, member):
 		# Shorthand for later use
