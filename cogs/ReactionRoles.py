90e0a7b5aa55e9a8b83544e73a3f3c7e25f835e1
jakob753951 <jlm1999@live.dk>
2021-05-27 17:31:04 +0000

    not 100% sure what i did, but it looks neat

--- cogs/ReactionRoles.py
+++ cogs/ReactionRoles.py
@@ -6,10 +6,29 @@ from attrdict import AttrDict
 
 requirements = {'general': [], 'server': []}
 
-def load_reaction_roles():
+def load_data():
 	with open('ReactionRoles.json', encoding='utf-8') as in_file:
 		data = json.load(in_file)
 	return data
+
+def save_data(data):
+	with open('ReactionRoles.json', 'w', encoding='utf-8') as in_file:
+		json.dump(data, in_file)
+
+def parse_role(ctx, role) -> discord.Role:
+	# Check if role is an id
+	if role in (role.id for role in ctx.guild.roles):
+		return next(role for role in ctx.guild.roles if role.id == role)
+
+	# Check if role matches a role name exactly
+	if role in (role.name for role in ctx.guild.roles):
+		return next(role for role in ctx.guild.roles if role.name == role)
+
+	# Check if role matches a role name case-insensitive
+	if role.lower() in (role.name.lower() for role in ctx.guild.roles):
+		return next(role.lower() for role in ctx.guild.roles if role.name.lower() == role.lower())
+	
+	raise KeyError
 
 class ReactionRoles(commands.Cog):
 	def __init__(self, bot):
@@ -27,6 +46,17 @@ class ReactionRoles(commands.Cog):
 		await ctx.send(embed=embed)
 		await ctx.message.delete()
 	
+	@is_admin()
+	@commands.command(name='AddReaction')
+	async def add_reaction(self, ctx, emoji, *, role: str):
+		try:
+			role = parse_role(ctx, role)
+		except KeyError:
+			await ctx.send('Role not found')
+			return
+
+		[ctx.message.id] = {'role_id': role.id, 'unique': False}
+	
 	@commands.Cog.listener()
 	async def on_raw_reaction_add(self, payload):
 		await self.handle_reaction(self, payload)
@@ -36,7 +66,7 @@ class ReactionRoles(commands.Cog):
 		await self.handle_reaction(self, payload)
 
 	async def handle_reaction(self, payload: discord.RawReactionActionEvent):
-		msgs = load_reaction_roles()
+		msgs = load_data()
 
 		if payload.message_id not in msgs:
 			return
