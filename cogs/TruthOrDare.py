7aa98d201cbbf4732c0cd4c2872732e3c4103d0d
jakob753951 <jlm1999@live.dk>
2021-05-28 05:36:23 +0000

    update to TruthOrDare json loading

--- cogs/TruthOrDare.py
+++ cogs/TruthOrDare.py
@@ -9,8 +9,8 @@ class TruthOrDare(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 		self.cfg = load_config('Config.json')
-		with open('data/TruthOrDare.json', 'r', encoding="utf8") as prompt_file:
-			self.questions = json.loads(prompt_file.read())
+		with open('data/TruthOrDare.json', encoding="utf8") as prompt_file:
+			self.questions = json.load(prompt_file)
 
 	@commands.command(name='TruthOrDare', aliases=['TorD', 'ToD'])
 	async def truth_or_dare(self, ctx, *, category = 'sfw'):
