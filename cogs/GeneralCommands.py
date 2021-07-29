03d0bbd1317305167c4eb9150a45be4ce033b8ce
jakob753951 <jlm1999@live.dk>
2021-07-29 21:45:51 +0000

    add checks for GeneralCommands

--- cogs/GeneralCommands.py
+++ cogs/GeneralCommands.py
@@ -4,6 +4,7 @@ from discord.ext import commands
 from discord.ext.commands.converter import TextChannelConverter
 from Configuration import load_config
 from asyncio import gather
+from CustomChecks import *
 
 requirements = {'general': [], 'server': ['react_confirm']}
 
@@ -40,28 +41,33 @@ class GeneralCommands(commands.Cog):
 		else:
 			await ctx.send("Yea.")
 		
+	@is_admin()
 	@commands.command(name='ban')
 	async def ban(self, ctx: commands.Context, user: discord.User, reason: str = None):
 		guild = ctx.guild
 		await guild.ban(user, reason=reason)
 		await ctx.send(embed=Embed(description=f'User {user.name} was banned.', color=0xff0000))
 		
+	@is_admin()
 	@commands.command(name='kick')
 	async def kick(self, ctx: commands.Context, user: discord.User, reason: str = None):
 		guild = ctx.guild
 		await guild.kick(user, reason=reason)
 		await ctx.send(embed=Embed(description=f'User {user.name} was kicked.', color=0xff0000))
 
+	@is_admin()
 	@commands.command(name='Say')
 	async def say(self, ctx, *args):
 		channel, message = await parse_args(ctx, args, self.cfg.react_confirm)
 		await channel.send(embed=Embed(description=message, color=0xff8000), files=ctx.message.attachments)
 
+	@is_admin()
 	@commands.command(name='SayNoEmbed', aliases=['NoEmbedSay'])
 	async def say_no_embed(self, ctx, *args):
 		channel, message = await parse_args(ctx, args, self.cfg.react_confirm)
 		await channel.send(message, files=ctx.message.attachments)
 	
+	@is_admin()
 	@commands.command(name='SendDM', aliases=['DMMember', 'DMUser'])
 	async def send_dm(self, ctx, recipient: discord.User, *, message):
 		await gather(
